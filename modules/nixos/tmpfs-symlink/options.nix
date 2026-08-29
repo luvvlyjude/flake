bcf6c0afcbd5db64c716d0064716ea2e41fa424b
jude <luvvlyjude@gmail.com>
2026-08-28 17:24:00 -0700

    tmpfs-symlink(fix): fix missing options attributes
    added missing descriptions and fixed a bug with invalid default option

--- modules/nixos/tmpfs-symlink/options.nix
+++ modules/nixos/tmpfs-symlink/options.nix
@@ -10,11 +10,11 @@ let
     submodule
     ;
 
-  relativePath = strMatching "^[^/].*" // {
+  relativePath = strMatching "[^/].*" // {
     description = "path relative to $HOME (not starting with '/')";
   };
 
-  mountSizeStr = strMatching "^[0-9]+([kKmMgG]|%)?$" // {
+  mountSizeStr = strMatching "[0-9]+([kKmMgG]|%)?" // {
     description = "tmpfs max usage size option string (eg. 4G, 15%, ...)";
   };
 in
@@ -25,7 +25,6 @@ in
     type = listOf (submodule ({
       options.saves = mkOption {
         type = relativePath;
-        default = "";
         example = ".local/share/PrismLauncher/instances/RSG Instance/minecraft/saves";
         description = ''
           Path to the instance's normal saves folder.
@@ -37,11 +36,11 @@ in
       options.size = mkOption {
         type = mountSizeStr;
         default = "4G";
-        example = "4G";
+        example = "15%";
         description = ''
           Max usage of system memory for this instance's tmpfs.
-            
-          noop unless this instance's `tmpfs` option set to `true`.
+
+          No effect unless this instance's `tmpfs` option set to `true`.
         '';
       };
 
@@ -54,14 +53,14 @@ in
 
           Because this option mounts tmpfs over the existing saves folder it
           can end up hiding persistent existing saves. They do not get deleted.
-          All worlds in the instance's tmpfs saves folder will be deleted apon
-          reboot and must be explicitly copied or moved to save them.
+          All worlds in the instance's tmpfs saves folder will be deleted upon
+          reboot and must be explicitly copied or moved to save them. Hidden
+          on-disk worlds can be recovered by unmounting the tmpfs.
         '';
       };
 
       options.user = mkOption {
         type = str;
-        default = "";
         example = "alice";
         description = ''
           The user whose $HOME to use for other path options and who will own the tmpfs mount.
@@ -71,7 +70,7 @@ in
       options.worlds = mkOption {
         type = listOf relativePath;
         default = [ ];
-        example = [ "mcsr/worlds/Instance Exclusive World" ];
+        example = [ "mcsr/worlds/Portal Practice v2" ];
         description = ''
           World folders linked into this instance.
 
@@ -79,5 +78,22 @@ in
         '';
       };
     }));
+    default = [ ];
+    example = [
+      {
+        saves = ".local/share/PrismLauncher/instances/RSG Instance/minecraft/saves";
+        size = "15%";
+        tmpfs = true;
+        user = "alice";
+        worlds = [
+          "mcsr/worlds/Portal Practice v2"
+        ];
+      }
+    ];
+    description = ''
+      List of instance submodules defining each instance's options
+
+      Each instance requires specifying `user` and `saves`.
+    '';
   };
 }
