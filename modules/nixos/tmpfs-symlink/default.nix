b65c492406a2f12697ca37d6fd3d14c89c5cad8c
jude <luvvlyjude@gmail.com>
2026-08-28 17:20:35 -0700

    tmpfs-symlink(fix): misc script improvements
    escape shell args properly with escapeShellArg, switch shell application
    to just shell script, and inline systemd dependency for systemctl call
    
    also fixed a bug with accidentally resolving a dead symlink (line 80)

--- modules/nixos/tmpfs-symlink/default.nix
+++ modules/nixos/tmpfs-symlink/default.nix
@@ -12,8 +12,8 @@ let
     concatMap
     concatMapStringsSep
     concatStringsSep
+    escapeShellArg
     filter
-    getExe
     mkIf
     ;
 
@@ -49,54 +49,48 @@ let
   # effectively clean up links that we dont declare anymore
   stateFilePath = "/var/lib/tmpfs-symlink/links";
 
-  stateFile = (
-    pkgs.writeText "tmpfs-symlink-all-links" (
-      concatStringsSep "\n" (map (world: world.link) allWorlds) + "\n"
-    )
+  stateFile = pkgs.writeText "tmpfs-symlink-all-links" (
+    concatStringsSep "\n" (map (world: world.link) allWorlds) + "\n"
   );
 
-  script = pkgs.writeShellApplication {
-    name = "tmpfs-symlink";
-    runtimeInputs = [
-      pkgs.coreutils
-      pkgs.systemd
-    ];
-    text = ''
-      # delete all links declared in the previous state file
-      if [ -f "${stateFilePath}" ]; then
-        while IFS= read -r line; do
-          if [ -L "$line" ]; then
-            echo "Removing link: $line"
-            rm -- "$line"
-          fi
-        done < "${stateFilePath}"
-      fi
-
-      # manually start the mount unit only after deleting links
-      # this way all on-disk links get deleted before mounting tmpfs
-      # if someone is enabling tmpfs for the first time it would otherwise
-      # leave behind links on disk
-      ${concatMapStringsSep "\n" (tmpfs: ''
-        systemctl start ${escapeSystemdPath tmpfs.where}.mount
-      '') allTmpfs}
-
-      # create all world links
-      ${concatMapStringsSep "\n" (world: ''
-        if [ -e "${world.link}" ]; then
-          echo "Warning: Something with the same name already exists at ${world.link}. Skipping..."
-        elif [ ! -d "$(dirname "${world.link}")" ]; then
-          echo "Warning: Parent directory does not exist for ${world.link}. Skipping..."
-        else
-          if [ ! -e "${world.target}" ]; then
-            echo "Warning: Target source does not exist for ${world.link}. Creating dead link..."
-          fi
-          ln -sT "${world.target}" "${world.link}"
+  script = pkgs.writeShellScript "tmpfs-symlink" ''
+    # delete all links declared in the previous state file
+    stateFilePath=${escapeShellArg stateFilePath}
+    if [ -f "$stateFilePath" ]; then
+      while IFS= read -r line; do
+        if [ -L "$line" ]; then
+          echo "Removing link: $line"
+          rm -- "$line"
         fi
-      '') allWorlds}
+      done < "$stateFilePath"
+    fi
 
-      install -Dm644 ${stateFile} ${stateFilePath}
-    '';
-  };
+    # manually start the mount unit only after deleting links
+    # this way all on-disk links get deleted before mounting tmpfs
+    # if someone is enabling tmpfs for the first time it would otherwise
+    # leave behind links on disk
+    ${concatMapStringsSep "\n" (tmpfs: ''
+      ${config.systemd.package}/bin/systemctl start "${escapeSystemdPath tmpfs.where}.mount"
+    '') allTmpfs}
+
+    # create all world links
+    ${concatMapStringsSep "\n" (world: ''
+      link=${lib.escapeShellArg world.link}
+      target=${lib.escapeShellArg world.target}
+      if [ -e "$link" ] || [ -L "$link" ]; then
+        echo "Warning: Something already exists at $link. Skipping..."
+      elif [ ! -d "$(dirname "$link")" ]; then
+        echo "Warning: Parent directory does not exist for $link. Skipping..."
+      else
+        if [ ! -e "$target" ]; then
+          echo "Warning: Target does not exist for $link. Creating dead link..."
+        fi
+        ln -sT "$target" "$link"
+      fi
+    '') allWorlds}
+
+    install -Dm644 ${stateFile} ${stateFilePath}
+  '';
 in
 {
   options.services.tmpfs-symlink = options;
@@ -115,7 +109,7 @@ in
       serviceConfig = {
         Type = "oneshot";
         StateDirectory = "tmpfs-symlink";
-        ExecStart = getExe script;
+        ExecStart = script;
       };
     };
 
