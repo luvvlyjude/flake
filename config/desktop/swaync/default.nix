febf00509d3b161c0a32a1df6f0144831d78d324
jude <luvvlyjude@gmail.com>
2026-09-07 05:31:16 -0700

    swaync(feat): prefer main monitor

--- config/desktop/swaync/default.nix
+++ config/desktop/swaync/default.nix
@@ -1,5 +1,10 @@
 {
   hm.services.swaync = {
     enable = true;
+
+    settings = {
+      "notification-window-preferred-output" = "DP-2";
+      "control-center-preferred-output" = "DP-2";
+    };
   };
 }
