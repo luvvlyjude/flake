7e616681939bccf584a9ec80c5d6b2ae91769f4c
jude <luvvlyjude@gmail.com>
2026-09-13 22:51:44 -0700

    bash(fix): variables double defined

--- config/programs/bash/default.nix
+++ config/programs/bash/default.nix
@@ -8,9 +8,6 @@
       _first_prompt_=1
       PROMPT_COMMAND='if [[ -n "$_first_prompt_" ]]; then unset _first_prompt_; else echo; fi'
       PS1="\[\033[1;38;5;53m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-
-      export EDITOR=nvim
-      export VISUAL=nvim
     '';
   };
 }
