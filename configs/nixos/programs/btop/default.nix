d21b2a411e3efd38c8efa84d3ace464a8aee5917
jude <luvvlyjude@gmail.com>
2026-09-26 02:54:42 -0700

    btop(feat): add cuda support

--- configs/nixos/programs/btop/default.nix
+++ configs/nixos/programs/btop/default.nix
@@ -1,11 +1,14 @@
 {
-  hm.programs.btop = {
-    enable = true;
-    settings = {
-      disks_filter = "exclude=/boot";
-      shown_boxes = "cpu proc mem";
-      swap_disk = false;
-      update_ms = 500;
+  hm = { pkgs, ... }: {
+    programs.btop = {
+      enable = true;
+      package = pkgs.btop-cuda;
+      settings = {
+        disks_filter = "exclude=/boot";
+        shown_boxes = "cpu proc mem";
+        swap_disk = false;
+        update_ms = 500;
+      };
     };
   };
 }
